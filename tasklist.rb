9e0f74a4b25a50e418e32d5abfd26ad512255bd0
Kevin Colyar <kevin@colyar.net>
2010-09-02 09:45:51 -0700

    Storing date in task

--- tasklist.rb
+++ tasklist.rb
@@ -1,20 +1,32 @@
+require 'date'
+
+class TaskList
+  @dates = []
+  
+  class << self
+    attr_accessor :dates
+  end
+end
+
 module Kernel
   def on(date, &block)
-    puts date
+    TaskList.dates << Date.parse(date)
     block.call
+    TaskList.dates.pop
   end
 
   def task(*args)
-    task = Task.new(args[0], args[1], args[2], args[3..-1] || [])
+    task = Task.new(TaskList.dates.last, args[0], args[1], args[2], args[3..-1] || [])
     puts task
   end
 
 end
 
 class Task
-  attr_accessor :name, :start_time, :end_end, :tags
+  attr_accessor :date, :name, :start_time, :end_end, :tags
 
-  def initialize(name, start_time, end_time, *tags)
+  def initialize(date, name, start_time, end_time, *tags)
+    @date = date
     @name = name
     @start_time = start_time
     @end_time = end_time
@@ -22,6 +34,6 @@ class Task
   end
 
   def to_s
-    "Task #{@name} #{@start_time} #{@end_time} #{@tags.join(',')}"
+    "#{@date} #{@name} #{@start_time} #{@end_time} #{@tags.join(',')}"
   end
 end
